6203d0b48ab1e4376e0a3ec60a933bd6eeaa3588
pr0gs-lab <60014443+pr0gs-lab@users.noreply.github.com>
2021-01-24 16:35:57 +0100

    Update artificial-intelligence.py

--- application/artificial-intelligence.py
+++ application/artificial-intelligence.py
@@ -1,1 +1,46 @@
-# The document is not available for the moment
+# The final version of this code is not yet available
+
+from random import randint
+import random
+
+status = "user" # Choice between user or admin statues
+
+if status == "admin":
+    transform = input("Your topics : ")
+else:
+    transform = "Animal Bricolage Décoration Education Informatique Loisir"
+    
+topics = transform.split(" ")
+
+class Favorite:
+    def __init__(self, topics):
+        self.topics = topics
+        self.nblike = dict.fromkeys(topics,0)
+        self.nbDislike = dict.fromkeys(topics, 0)
+        
+    def __repr__(self):
+        return f"{self.topics}"
+    
+    def trier(self):
+        n = 5
+        while True:
+            doit_continuer = False
+            i = random.choice(self.topics)
+            for search in topics:
+                if self.nblike[search] + self.nbDislike[search] < 5:
+                    doit_continuer = True
+            answer = input(f"{i} | Like or no like : ")
+            if answer == "like":
+                self.nblike[i] += 1
+            elif answer == "no":
+                self.nbDislike[i] += 1
+            if doit_continuer == False:
+                break
+            
+            
+
+        print(f"Like : {self.nblike}")
+        print(f"Dislike : {self.nbDislike}")
+        
+    
+user = Favorite(topics)
